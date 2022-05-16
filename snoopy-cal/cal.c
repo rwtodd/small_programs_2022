273c83deef147f8ad7a745e7e9bad58d5079e7dd
Richard Todd <rwtodd@users.noreply.github.com>
2022-05-16 00:22:28 -0500

    preparing to draw the day grid

--- snoopy-cal/cal.c
+++ snoopy-cal/cal.c
@@ -1,5 +1,6 @@
 /* http://gunkies.org/wiki/Snoopy_Calendar#snpcal.dat */
 #include<stdio.h>
+#include<time.h>
 #include<assert.h>
 
 #include "picdata.inc"
@@ -49,6 +50,65 @@ draw_month_banner(int year, int month_idx)
   #undef MONTH
 }
 
+/* draw a month with `tot_days` days, that starts on day `idx` */
+void
+draw_day_grid(int tot_days, int idx)
+{
+   assert(tot_days >= 28 && tot_days <= 31);
+   assert(idx >= 0 && idx <= 6);
+
+   static const char blank[1] = "";
+   putchar('\n'); puts(weekdays); putchar('\n');
+
+   idx = -idx + 1;
+   signed char digits[14];
+   while(idx <= tot_days)
+     {
+        /* fill out digits[] with a row of 14 potential digits to draw */
+        for(int column = 0; column < 7; ++column, ++idx)
+          {
+            if(idx <= 0 || idx > tot_days) { digits[column*2] = -1; digits[column*2+1] = -1; }
+            else if(idx < 10) { digits[column*2] = -1; digits[column*2+1] = (char)idx; }
+            else { digits[column*2] = (char)(idx/10); digits[column*2+1] = (char)(idx%10); }
+          }
+         
+        /* draw the digits */
+        for(int row = 0; row < 7; ++row)
+          {
+              /* TODO! */
+          }
+     }
+}
+
+/*
+         SUNDAY            MONDAY            TUESDAY          WEDNESDAY         THURSDAY           FRIDAY           SATURDAY        
+                                                                                                                                    
+                     I                 I                 I                 I                 I                 I                    
+                     I                 I                 I                 I                 I           1     I          222       
+                     I                 I                 I                 I                 I          11     I         2   2      
+                     I                 I                 I                 I                 I           1     I            2       
+                     I                 I                 I                 I                 I           1     I          2         
+                     I                 I                 I                 I                 I         11111   I         22222      
+                     I                 I                 I                 I                 I                 I                    
+ --------------------I-----------------I-----------------I-----------------I-----------------I-----------------I--------------------
+                     I                 I                 I                 I                 I                 I                    
+             33333   I            4    I         55555   I          666    I         77777   I          888    I          999       
+                 3   I           44    I         5       I         6       I             7   I         8   8   I         9   9      
+               33    I          4 4    I         5555    I         6666    I            7    I          888    I          9999      
+             3   3   I         44444   I             5   I         6   6   I           7     I         8   8   I             9      
+              333    I            4    I         5555    I          666    I           7     I          888    I          999       
+                     I                 I                 I                 I                 I                 I                    
+ --------------------I-----------------I-----------------I-----------------I-----------------I-----------------I--------------------
+                     I                 I                 I                 I                 I                 I                    
+        1     000    I    1      1     I    1     222    I    1    33333   I    1       4    I    1    55555   I    1     666       
+       11    0   0   I   11     11     I   11    2   2   I   11        3   I   11      44    I   11    5       I   11    6          
+        1    0   0   I    1      1     I    1       2    I    1      33    I    1     4 4    I    1    5555    I    1    6666       
+        1    0   0   I    1      1     I    1     2      I    1    3   3   I    1    44444   I    1        5   I    1    6   6      
+      11111   000    I  11111  11111   I  11111  22222   I  11111   333    I  11111     4    I  11111  5555    I  11111   666       
+                     I                 I                 I                 I                 I                 I                    
+ --------------------I-----------------I-----------------I-----------------I-----------------I-----------------I--------------------
+                     I
+*/
 
 int 
 main(int argc, char *argv[])
@@ -56,13 +116,19 @@ main(int argc, char *argv[])
   int year = 1969;
   int month_idx = 1;
 
-  if(argc > 1) sscanf(argv[1],"%d",&year);
-  if(argc > 2) sscanf(argv[2],"%d",&month_idx);
+  /* First, handle command-line args and validate */
+  switch(argc) {
+  case 0: case 1: break;
+  case 2: sscanf(argv[1],"%d",&month_idx); break;
+  case 3: sscanf(argv[1],"%d",&year); sscanf(argv[2],"%d",&month_idx); break;
+  }
+
   --month_idx;
   if (month_idx < 0 || month_idx > 12 || year > 9999 || year < 0 ||
-		  (month_idx == 12 && year == 9999))
+		  (month_idx == 12 && year == 9999) || argc > 3)
     {
-      fprintf(stderr,"Usage: %s year month\n", argc > 0 ? argv[0] : "snoopy-cal");
+      fprintf(stderr,"Usage: %s [month]\n", argc > 0 ? argv[0] : "snoopy-cal");
+      fprintf(stderr,"   or: %s year month\n", argc > 0 ? argv[0] : "snoopy-cal");
       fprintf(stderr,"  year can be 0 to 9999\n");
       fprintf(stderr,"  month can be 1 to 13 (13 is Jan of following year)\n");
       return -1;
@@ -85,5 +151,6 @@ main(int argc, char *argv[])
    */
   if (month_idx == 12) month_idx = 0, ++year;
   draw_month_banner(year, month_idx);
+  draw_day_grid(31,6);
   return 0;
 }
