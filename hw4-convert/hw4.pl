0ab17f908920dfdb51a37809c11eb9c0c216cd1b
Richard Todd <rwtodd@users.noreply.github.com>
2022-06-18 22:27:09 -0500

    reading sections now

--- hw4-convert/hw4.pl
+++ hw4-convert/hw4.pl
@@ -13,36 +13,37 @@ open_input(FName,IStrm) :- open(FName, read, IStrm, [type(binary)]).
 close_input(IStrm) :- close(IStrm).
 
 % read in the list of image files (.PCX)
-parse_filedir(Fdir,FDir). % TODO!
+parse_filedir(FDir,FDir). % TODO!
 read_files_directory(IS, FDir) :-
   seek(IS,bof, 0x1c380, 0x1c380),
   read_section(IS,Entries),
   parse_filedir(Entries, FDir).
 
 % parse book contents
-parse_until(IStrm, Finish, []) :- stream_position(IStrm,Finish), !.
-parse_until(IStrm, Finish, [B|Bs]) :-
-  read_section(IStrem, B), !,
-  parse_until(IStrm, Finish, Bs).
-
-parse_book(IStrm, Start, Finish, Book) :-
+parse_book(IStrm, _, _, Start, Finish) :-
+  format('%X\n',[Finish]), % TODO
   seek(IStrm, bof, Start, Start),
-  parse_until(IStrm, Finish, Book). 
+  repeat,
+    read_section(IStrm, Sect),
+    length(Prefix, 16), append(Prefix,_,Sect), format('%X: ~p\n',[Location,Prefix]), % TODO!
+    seek(IStrm, current, 0, Location),
+    Location >= Finish.
+
 
 % conveniences...
 fname(formats_collection,'~/win/Downloads/DDJFORM.HW4').
 fname(algos_collection,'~/win/Downloads/ALGO.HW4').
 
-book_span(file_formats, 0x4bb40, 0xe45f0).
-book_span(graphics_formats, 0xe4bc0, 0x01b3a80).
-book_span(windows_formats, 0x1b3e80, 0). % TODO!
+book_info(file_formats, 'ff_', 0x4bb40, 0xe4600).
+book_info(graphics_formats, 'gf_', 0xe4bc0, 0x01b3a80).
+book_info(windows_formats, 'wf_', 0x1b3e80, 0). % TODO!
 
 main :-
   fname(formats_collection,Fname),
   open_input(Fname,IS),
   read_files_directory(IS,FDir),
-  book_span(file_formats, Start, Finish),
-  parse_book(IS, Start, Finish, _),
+  book_info(file_formats, Prefix, Start, Finish),
+  parse_book(IS, Prefix, FDir, Start, Finish),
   close_input(IS).
 
 % vim: filetype=prolog : sw=2 : ts=2 : sts=2 : expandtab :
