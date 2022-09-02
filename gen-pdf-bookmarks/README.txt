94007c22da18b79702ee77b944e45edf9ea46751
Richard Todd <rwtodd@users.noreply.github.com>
2022-09-02 16:01:38 -0500

    gen-bmark now handles page labels... should probably rewrite in perl/python/etc

--- gen-pdf-bookmarks/README.txt
+++ gen-pdf-bookmarks/README.txt
@@ -1,4 +1,42 @@
-A helper program to generate pdf bookmark data
+A helper program to generate pdf bookmark and page label data
 for use by pdftk-java.
 
+Because it's a simple C program, the format is pretty rigid,
+but for whatever reason I didn't feel like using perl/python today.
+
+The following commands are accepted (1 per line):
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+1) Name a page with text:  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+NAME 1: Cover
+NAME 210: BackCover
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+2) Number pages to match the book (decimal numbers): ~~~~~~~~~~~~~
+
+PAGE 2 = BOOK 1  
+PAGE 40 = BOOK 52  and BTW this text will be ignored
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+3) Number pages to match the book (roman numerals): ~~~~~~~~~~~~~~
+
+PAGE 2 = BOOK 1r
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+4) Add a bookmark (the page number is the BOOK page according to
+the most-recent 'PAGE' command) ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+4 Contents
+6 Preface
+8 Chapter One
+ 10 Subheading 1
+ 12 Subheading 2
+14 Chapter Two
+ 15 Indenting makes nested bookmarks like you might expect
+ 21 So these two items are "under" Chapter Two in most PDF readers
+  22 So this is a third-level of nesting
+  24 Only indent by 1 space at a time... it's a rigid simple C program
+28 Chapter Three
 
--- gen-pdf-bookmarks/gen-bmark.c
+++ gen-pdf-bookmarks/gen-bmark.c
@@ -6,26 +6,57 @@ int
 main(int argc, char **argv)
 {
   char buf[256];  /* good enough for this toy program */
+
+  /* we need to store labels text and output it at the end... so ... */
+  char labels[1024];
+  labels[0] = '\0';
+  size_t label_sz = 0;
+
   int poffs = 0;  /* the current page offset */
 
-  /* now read all the chapters */
+  /* keep reading until we run out of data */
   while(1) {
     int indent = -1,pageno = -1;
-    /* always check for a new poffs offset ... */
     int pdfpg, bookpg;
+
+    /* check for book numbering commands */
     if(scanf("PAGE %d = BOOK %d",&pdfpg,&bookpg) == 2) {
+       /* check for roman numerals */
+       char * style = (scanf("%[rR]",buf) == 1) ? "LowercaseRomanNumerals" : "DecimalArabicNumerals";
+
        /* eat the rest of the line the CR/LF */
        if( scanf("%*[^\r\n]") < 0 || scanf("%*[\r\n]") < 0) 
          err_exit("error on PAGE=BOOK line???");
        poffs = pdfpg - bookpg;
+       int len = sprintf(labels + label_sz, 
+          "PageLabelBegin\nPageLabelNewIndex: %d\nPageLabelStart: %d\nPageLabelNumStyle: %s\n",
+          pdfpg, bookpg, style);
+       if(len < 0) err_exit("error formatting page labels!");
+       label_sz += len;
     }
 
-    /* ... and then read the next contents entry */
-    int result = scanf(" %n%d %[^\r\n]%*[\r\n]",&indent,&pageno,buf);
-    if(result != 2)
-      if(indent > 0 || pageno != -1) { err_exit("bad input!"); }
-      else break;  
-    printf("BookmarkBegin\nBookmarkTitle: %s\nBookmarkLevel: %d\nBookmarkPageNumber: %d\n",
-        buf, indent+1, pageno+poffs);
+    /* check for a new named page (sets offset back to 0) */
+    else if(scanf("NAME %d: %[^\r\n]%*[\r\n]",&pdfpg,buf) == 2) {
+       poffs = 0;
+       int len = sprintf(labels + label_sz, 
+          "PageLabelBegin\nPageLabelNewIndex: %d\nPageLabelStart: 1\nPageLabelPrefix: %s\nPageLabelNumStyle: NoNumber\n",
+          pdfpg, buf);
+       if(len < 0) err_exit("error formatting page labels!");
+       label_sz += len;
+       continue;
+    }
+
+    /* if we got here we better have a bookmark */
+    else {
+      int result = scanf(" %n%d %[^\r\n]%*[\r\n]",&indent,&pageno,buf);
+      if(result != 2)
+        if(indent > 0 || pageno != -1) { err_exit("bad input!"); }
+        else break;  
+      printf("BookmarkBegin\nBookmarkTitle: %s\nBookmarkLevel: %d\nBookmarkPageNumber: %d\n",
+          buf, indent+1, pageno+poffs);
+    }
   }
+
+  /* write the saved page labels so they appear in a block at the end */
+  puts(labels);
 }
