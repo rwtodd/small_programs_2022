cd825bbcdab8312228a887af74e7a17dadf7646e
Richard Todd <rwtodd@users.noreply.github.com>
2022-09-02 13:37:49 -0500

    fancier offset handling

--- gen-pdf-bookmarks/gen-bmark.c
+++ gen-pdf-bookmarks/gen-bmark.c
@@ -6,14 +6,22 @@ int
 main(int argc, char **argv)
 {
   char buf[256];  /* good enough for this toy program */
-  /* first read the offset for pages */
-  int poffs = 0;
-  if(scanf("OFFSET %d%*[\n]",&poffs) < 0) err_exit("problem reading the first line!");
+  int poffs = 0;  /* the current page offset */
 
   /* now read all the chapters */
   while(1) {
     int indent = -1,pageno = -1;
-    int result = scanf(" %n%d %[^\n]%*c",&indent,&pageno,buf);
+    /* always check for a new poffs offset ... */
+    int pdfpg, bookpg;
+    if(scanf("PAGE %d = BOOK %d",&pdfpg,&bookpg) == 2) {
+       /* eat the rest of the line the CR/LF */
+       if( scanf("%*[^\r\n]") < 0 || scanf("%*[\r\n]") < 0) 
+         err_exit("error on PAGE=BOOK line???");
+       poffs = pdfpg - bookpg;
+    }
+
+    /* ... and then read the next contents entry */
+    int result = scanf(" %n%d %[^\r\n]%*[\r\n]",&indent,&pageno,buf);
     if(result != 2)
       if(indent > 0 || pageno != -1) { err_exit("bad input!"); }
       else break;  
