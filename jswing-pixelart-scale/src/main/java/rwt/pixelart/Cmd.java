d193204535b47180f4003d62746eb848b8abebcd
Richard Todd <rwtodd@users.noreply.github.com>
2022-12-06 00:30:28 -0600

    made the code work with any compatibleimage

--- jswing-pixelart-scale/src/main/java/rwt/pixelart/Cmd.java
+++ jswing-pixelart-scale/src/main/java/rwt/pixelart/Cmd.java
@@ -1,10 +1,6 @@
 package rwt.pixelart;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -44,9 +40,16 @@ public class Cmd extends JFrame {
 	private int c2 = 0xff_ff_00_00;
 
 	private void drawImage() {
+		//getDataElements takes an ARGB value and converts it to what the raster wants...
+		final var col1 = bi.getColorModel().getDataElements(c1,null);
+		final var col2 = bi.getColorModel().getDataElements(c2, null);
+		final var raster = bi.getRaster();
+
 		for(int y = 0; y < bi.getHeight(); ++y)
 			for(int x = 0; x < bi.getWidth(); ++x)
-				bi.setRGB(x,y, (((x+y)&1)==0)? c1 : c2);
+				// ... and raster.setDataElements takes the colormodel output and sets the value in the raster
+				raster.setDataElements(x,y, ((x+y)&1) == 0 ? col1 : col2);
+
 		c1 += 4;
 		c2 += 4;
 		if(c1 >= 0xff_00_00_f1) c1 = 0xff_00_00_00;
@@ -56,8 +59,9 @@ public class Cmd extends JFrame {
 	public Cmd() {
 		super("Pixel-Perfext");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		bi = new BufferedImage(160, 200, BufferedImage.TYPE_INT_ARGB_PRE);
+
+		// we create the ideal image type for our display...
+		bi = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleImage(160,200);
 	    drawImage();
 
 		jp = new JPanel() {
