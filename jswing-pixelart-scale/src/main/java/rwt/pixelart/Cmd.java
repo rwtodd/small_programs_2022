a00f03e076fe6831e6770c3a29114154f650b73e
Richard Todd <rwtodd@users.noreply.github.com>
2022-12-06 12:14:30 -0600

    now constrains the canvas to 4:3 at all times

--- jswing-pixelart-scale/src/main/java/rwt/pixelart/Cmd.java
+++ jswing-pixelart-scale/src/main/java/rwt/pixelart/Cmd.java
@@ -3,9 +3,6 @@ package rwt.pixelart;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 import javax.swing.*;
@@ -21,7 +18,7 @@ public class Cmd extends JFrame {
 
 	private static final long serialVersionUID = -7835865311308198512L;
 
-	private JComponent jp;
+	private JComponent pixart;
 	
 	private final BufferedImage bi;
 	private Random rng = new Random();
@@ -37,12 +34,14 @@ public class Cmd extends JFrame {
 //	}
 
 	private int c1 = 0xff_00_00_00;
+	private Object col1 =null;
 	private int c2 = 0xff_ff_00_00;
+	private Object col2 = null;
 
 	private void drawImage() {
 		//getDataElements takes an ARGB value and converts it to what the raster wants...
-		final var col1 = bi.getColorModel().getDataElements(c1,null);
-		final var col2 = bi.getColorModel().getDataElements(c2, null);
+		col1 = bi.getColorModel().getDataElements(c1, col1);
+		col2 = bi.getColorModel().getDataElements(c2, col2);
 		final var raster = bi.getRaster();
 
 		for(int y = 0; y < bi.getHeight(); ++y)
@@ -50,32 +49,63 @@ public class Cmd extends JFrame {
 				// ... and raster.setDataElements takes the colormodel output and sets the value in the raster
 				raster.setDataElements(x,y, ((x+y)&1) == 0 ? col1 : col2);
 
-		c1 += 4;
-		c2 += 4;
-		if(c1 >= 0xff_00_00_f1) c1 = 0xff_00_00_00;
-		if(c2 >= 0xff_ff_00_f1) c2 = 0xff_ff_00_00;
+		c1 += rng.nextInt(8);
+		c2 += rng.nextInt(8);
+		if(c1 >= 0xff_00_01_00) c1 = 0xff_00_00_00;
+		if(c2 >= 0xff_ff_01_00) c2 = 0xff_ff_00_00;
 	}
 
 	public Cmd() {
-		super("Pixel-Perfext");
+		super("Pixel-Perfect");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
 		// we create the ideal image type for our display...
 		bi = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().createCompatibleImage(160,200);
 	    drawImage();
 
-		jp = new JComponent() {
+		pixart = new JComponent() {
 			{ setPreferredSize(new Dimension( 160*2*3, 240*3)); }
 			
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
+				// ok paint the largest 4:3 image possible in the space we have...
+				final int totalWidth = pixart.getWidth();
+				final int totalHeight = pixart.getHeight();
+
+				// use height/width 3:4 instead of width/height 4:3 because 0.75 is nicer to type exactly in base 10.
+				final float totalAR = (float)totalHeight/(float)totalWidth;
+				int w43, h43;
+				if(totalAR >= 0.75) {
+					// case 1: totalHeight too tall... totalAR >= 0.75 ...
+					w43 = totalWidth;
+					h43 = (int)(0.75*totalWidth);
+				} else {
+					// case 2: totalWidth is too wide... totalAR < 0.75
+					w43 = (int)(1.3333333f*totalHeight);
+					h43 = totalHeight;
+				}
+
+				// we want to center the canvas...
+				final int xoffs = (totalWidth - w43)/2;
+				final int yoffs = (totalHeight - h43)/2;
+
 				Graphics2D g2 = (Graphics2D)g;
+				// paint black borders...
+				g.setColor(Color.BLACK);
+				if(h43 < totalHeight) {
+					g.fillRect(0, 0, totalWidth, yoffs);
+					g.fillRect(0, yoffs + h43, totalWidth, totalHeight - yoffs - h43);
+				} else if(w43 < totalWidth) {
+					g.fillRect(0,0,xoffs,totalHeight);
+					g.fillRect(xoffs+w43,0,totalWidth-xoffs-w43, totalHeight);
+				}
+				// now paint the image...
 				g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-				g2.drawImage(bi, 0, 0, jp.getWidth(),jp.getHeight(), null);
+				g2.drawImage(bi, xoffs, yoffs, w43, h43, null);
 			}
 		};
-		setContentPane(jp);
+		setContentPane(pixart);
 		pack();
 		setVisible(true);
 		new javax.swing.Timer(1000/60, new ActionListener() {
